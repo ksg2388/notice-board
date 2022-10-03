d9ba335ac32f3cd6170130762e642016f3599394
ksg2388 <ksg2388@naver.com>
2022-10-03 14:14:10 +0900

    feat(login): login publishing

---
+++ suriname/src/routes/containers/LoginNavigationContainer.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LoginContainer from "../../components/Login/containers/LoginContainer";
+
+const LoginNavigationContainer = () => {
+  return <LoginContainer />;
+};
+
+export default LoginNavigationContainer;
