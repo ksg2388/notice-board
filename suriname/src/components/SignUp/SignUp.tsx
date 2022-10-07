092d0f531f106610d4e1f6b22fc715e89c5747fc
SeungGyu <tmdrb0912@naver.com>
2022-10-07 14:45:07 +0900

    feat(setting) : 절대경로 설정

--- suriname/src/components/SignUp/SignUp.tsx
+++ suriname/src/components/SignUp/SignUp.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import "./styles/signup.css";
+import React from 'react';
+import './styles/signup.css';
 
 type Props = {
   id: string;
@@ -39,63 +39,61 @@ const SingUp = ({
   handleName,
 }: Props) => {
   return (
-    <div className="signup-root">
-      <section className="signup-title-section">
-        <h1 className="title">식사는 잡쉈어??</h1>
-        <h3 className="sub-title">아니, 회원가입부터 하고~</h3>
+    <div className='signup-root'>
+      <section className='signup-title-section'>
+        <h1 className='title'>식사는 잡쉈어??</h1>
+        <h3 className='sub-title'>아니, 회원가입부터 하고~</h3>
       </section>
-      <section className="signup-input-section">
-        <form className="signup-form">
+      <section className='signup-input-section'>
+        <form className='signup-form'>
           <label>아이디</label>
           <input
             value={id}
-            type="text"
+            type='text'
             onChange={(e) => handleId(e.target.value)}
           />
           {id.length > 0 && (
-            <span className={`message ${isId ? "success" : "error"}`}>
+            <span className={`message ${isId ? 'success' : 'error'}`}>
               {idMessage}
             </span>
           )}
           <label>비밀번호</label>
           <input
             value={password}
-            type="password"
+            type='password'
             onChange={(e) => handlePassword(e.target.value)}
           />
           {password.length > 0 && (
-            <span className={`message ${isPassword ? "success" : "error"}`}>
+            <span className={`message ${isPassword ? 'success' : 'error'}`}>
               {passwordMessage}
             </span>
           )}
           <label>비밀번호 확인</label>
           <input
             value={passwordConfirm}
-            type="password"
+            type='password'
             onChange={(e) => handlePasswordConfirm(e.target.value)}
           />
           {passwordConfirm.length > 0 && (
             <span
-              className={`message ${isPasswordConfirm ? "success" : "error"}`}
-            >
+              className={`message ${isPasswordConfirm ? 'success' : 'error'}`}>
               {passwordConfirmMessage}
             </span>
           )}
           <label>닉네임</label>
           <input
-            type="text"
+            type='text'
             value={name}
             onChange={(e) => handleName(e.target.value)}
           />
           {name.length > 0 && (
-            <span className={`message ${isName ? "success" : "error"}`}>
+            <span className={`message ${isName ? 'success' : 'error'}`}>
               {nameMessage}
             </span>
           )}
           <button
-            className="signup-btn"
-            disabled={!(isName && isId && isPassword && isPasswordConfirm)}
-          >
+            className='signup-btn'
+            disabled={!(isName && isId && isPassword && isPasswordConfirm)}>
             가입하기
           </button>
         </form>
